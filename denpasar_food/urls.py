61572acf9b476496a6c581a801ec5076871ead3e
ferdinand <ferdinandbs77@gmail.com>
2024-10-24 04:48:49 +0700

    added basic admin dashboard, will improve later

--- denpasar_food/urls.py
+++ denpasar_food/urls.py
@@ -17,10 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from restaurants.views import RestaurantListView
+from admin_dashboard.views import admin_dashboard_restaurant_list, admin_dashboard_restaurant_create, admin_dashboard_restaurant_update, admin_dashboard_restaurant_delete, admin_dashboard_restaurant_batch_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RestaurantListView.as_view(), name='restaurant_list'),
     path('', include('authentication.urls')),
     # Add other URL patterns as needed
+    path('adminDashboard/restaurants/', admin_dashboard_restaurant_list, name='admin_dashboard_restaurant_list'),
+    path('adminDashboard/restaurants/add/', admin_dashboard_restaurant_create, name='admin_dashboard_restaurant_create'),
+    path('adminDashboard/restaurants/update/<int:pk>/', admin_dashboard_restaurant_update, name='admin_dashboard_restaurant_update'),
+    path('adminDashboard/restaurants/delete/<int:pk>/', admin_dashboard_restaurant_delete, name='admin_dashboard_restaurant_delete'),
+    
+    # Batch delete path
+    path('adminDashboard/delete/', admin_dashboard_restaurant_batch_delete, name='admin_dashboard_restaurant_batch_delete'),
+    
 ]
