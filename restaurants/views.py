8268e5d5f0e3628f9710760a61658183087b428f
isaacboentoro <isaac.jesse@ui.ac.id>
2024-10-25 17:07:47 +0700

    add select2 cuisine search

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,7 @@ from .forms import RestaurantFilterForm
 import logging
 import random
 import json
+from itertools import chain
 
 logger = logging.getLogger(__name__)
 
@@ -20,13 +21,23 @@ class RestaurantListView(ListView):
         queryset = super().get_queryset()
         search = self.request.GET.get('search')
         search_by = self.request.GET.get('search_by', 'name')
+        selected_cuisines = self.request.GET.getlist('cuisines')
+
+        logger.debug(f"Search: {search}")
+        logger.debug(f"Search by: {search_by}")
+        logger.debug(f"Selected cuisines: {selected_cuisines}")
 
         if search:
             if search_by == 'name':
                 queryset = queryset.filter(name__icontains=search)
             elif search_by == 'cuisine':
-                queryset = queryset.filter(cuisine__icontains=search)
+                queryset = queryset.filter(cuisines__icontains=search)
 
+        if selected_cuisines:
+            for cuisine in selected_cuisines:
+                queryset = queryset.filter(cuisines__icontains=cuisine)
+
+        logger.debug(f"Queryset count after filtering: {queryset.count()}")
         return queryset
 
     def get_context_data(self, **kwargs):
@@ -36,19 +47,33 @@ class RestaurantListView(ListView):
         context['featured_restaurants'] = featured_restaurants
         context['total_restaurants'] = Restaurant.objects.count()
         context['form'] = RestaurantFilterForm(self.request.GET or None)
+        
+        # Flatten the list of cuisines
+        all_cuisines = Restaurant.objects.values_list('cuisines', flat=True)
+        flattened_cuisines = set(chain.from_iterable(all_cuisines))
+        context['all_cuisines'] = flattened_cuisines
+        
         return context
 
 def restaurant_list_ajax(request):
     search_by = request.GET.get('search_by', 'name')
     search_query = request.GET.get('search', '')
+    selected_cuisines = request.GET.getlist('cuisines')
     logger.info(f"Search by: {search_by}")
     logger.info(f"Search query: {search_query}")
+    logger.info(f"Selected cuisines: {selected_cuisines}")
 
     queryset = Restaurant.objects.all()
 
     if search_query:
         filter_kwargs = {f"{search_by}__icontains": search_query}
         queryset = queryset.filter(**filter_kwargs)
+
+    if selected_cuisines:
+        for cuisine in selected_cuisines:
+            queryset = queryset.filter(cuisines__icontains=cuisine)
+
+    logger.info(f"Queryset count after filtering: {queryset.count()}")
 
     paginator = Paginator(queryset, 9)  # Show 9 restaurants per page
 
