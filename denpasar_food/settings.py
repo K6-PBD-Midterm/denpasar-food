b61b458b7e9b4e8d3a908f1a9192aa419d29e09a
ferdinand <ferdinandbs77@gmail.com>
2024-10-26 09:45:36 +0700

    implement the account management for admin

--- denpasar_food/settings.py
+++ denpasar_food/settings.py
@@ -138,5 +138,3 @@ STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_URL = '/auth/login/'
