1881bd350602caedd7d99af8d1b92f6dcfb48a31
adienanimeesha <adiena.nimeesha@gmail.com>
2024-10-26 20:26:10 +0700

    like

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required  # Make sure users are logged in to review
-from .models import Restaurant, Review
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Restaurant, Review, Like
 from .forms import ReviewForm
 
 @login_required
@@ -21,5 +22,27 @@ def add_review(request, restaurant_id):
 
 def restaurant_detail(request, restaurant_id):
     restaurant = get_object_or_404(Restaurant, id=restaurant_id)
-    reviews = restaurant.restaurant_reviews.all()  
-    return render(request, 'restaurant_detail.html', {'restaurant': restaurant, 'reviews': reviews})
+    reviews = restaurant.restaurant_reviews.all()
+    user_has_liked = restaurant.likes.filter(user=request.user).exists() if request.user.is_authenticated else False
+    return render(request, 'restaurant_detail.html', {'restaurant': restaurant, 'reviews': reviews, 'user_has_liked': user_has_liked})
+
+@login_required
+def like_restaurant(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    like, created = Like.objects.get_or_create(user=request.user, restaurant=restaurant)
+    if created:
+        messages.success(request, f'You have liked {restaurant.name}.')
+    else:
+        messages.info(request, f'You already liked {restaurant.name}.')
+    return redirect('restaurant_detail', restaurant_id=restaurant.id)
+
+@login_required
+def dislike_restaurant(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    like = Like.objects.filter(user=request.user, restaurant=restaurant).first()
+    if like:
+        like.delete()
+        messages.success(request, f'You have disliked {restaurant.name}.')
+    else:
+        messages.info(request, f'You have not liked {restaurant.name}.')
+    return redirect('restaurant_detail', restaurant_id=restaurant.id)
